1ee98ebd941893a7349f56eb1fca0ca281168723
Rahul Mool <rahulmool3@gmail.com>
2020-03-02 03:03:32 +0530

    Added Login to mobile_navigation

---
+++ app/src/main/java/com/example/internshipdemo4/ui/login/LoginViewModel.java
@@ -0,0 +1,20 @@
+package com.example.internshipdemo4.ui.login;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public LoginViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Login fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
+
