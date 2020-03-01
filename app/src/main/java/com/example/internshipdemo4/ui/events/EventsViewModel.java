6a541934685ad6b8d044680790caa7a6fdcd66a4
Rahul Mool <rahulmool3@gmail.com>
2020-03-02 01:26:47 +0530

    Added Events to mobile_navigation

---
+++ app/src/main/java/com/example/internshipdemo4/ui/events/EventsViewModel.java
@@ -0,0 +1,18 @@
+package com.example.internshipdemo4.ui.events;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class EventsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public EventsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
