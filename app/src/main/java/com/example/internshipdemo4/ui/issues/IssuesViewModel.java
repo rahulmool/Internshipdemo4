4dfd916a940343f8a62f0ccdc4ea675ba5aa0005
Rahul Mool <rahulmool3@gmail.com>
2020-03-02 01:12:06 +0530

    Added Issues to mobile_navigation

---
+++ app/src/main/java/com/example/internshipdemo4/ui/issues/IssuesViewModel.java
@@ -0,0 +1,19 @@
+package com.example.internshipdemo4.ui.issues;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class IssuesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public IssuesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
